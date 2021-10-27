85ecbd6270c166f4ccde8f941f47e90f7a96456c
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2021-10-27 14:55:46 +0700

    store all result to respective array

--- json-manipulation/app.js
+++ json-manipulation/app.js
@@ -5,50 +5,70 @@ fetch('data.json')
         // find items in meeting room
         console.log('========================================');
         console.log('All Items in Meeting Room');
+
+        const meetingRoomItems = [];
         for (let item of data) {
             if (item.placement.name === 'Meeting Room') {
-                console.log(item.name);
+                meetingRoomItems.push(item.name);
             }
         }
+        console.log(meetingRoomItems);
+
+
 
         // find all electronic devices
         console.log('========================================');
         console.log('All Elecronic Devices');
+
+        const electronicItems = [];
         for (let item of data) {
             if (item.type === 'electronic'){
-                console.log(item.name);
+                electronicItems.push(item.name);
             }
         }
-        
+        console.log(electronicItems);
+
+
         // find all furniture
         console.log('========================================');
         console.log('All Furniture');
+
+        const furnitureItems = [];
         for (let item of data) {
             if (item.type === 'furniture') {
-                console.log(item.name);
+                furnitureItems.push(item.name);
             }
         }
+        console.log(furnitureItems);
+
 
         // find all items were purchased on 16 Januari 2020
         console.log('========================================');
         console.log('All Items were Purchased on 16 Januari 2020');
+
+        const purchasedItems = []
         for (let item of data) {
             const unixTime = item.purchased_at;
             const date = new Date(unixTime * 1000).toLocaleDateString('in-ID');
             
             if (date === '16/1/2020') {
-                console.log(item.name);
+                purchasedItems.push(item.name);
             }
         }
-        
+        console.log(purchasedItems);
+
+
         // find all items with brown color
         console.log('========================================');
         console.log('Find All Items with Brown Color');
+
+        const brownItems = [];
         for (let item of data) {
             for (let tag of item.tags){
                 if (tag === 'brown') {
-                    console.log(item.name);
+                    brownItems.push(item.name);
                 }
             }
         }
+        console.log(brownItems);
     })
