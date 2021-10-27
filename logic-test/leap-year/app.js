b938198ce961fc5531911c28621ffc1465c4eb4a
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2021-10-27 13:32:00 +0700

    added a few comment on leap-year

--- logic-test/leap-year/app.js
+++ logic-test/leap-year/app.js
@@ -1,8 +1,12 @@
 function isLeapYear(startYear, endYear) {
     const leapYear = [];
+
     for (let currentYear = startYear; currentYear <= endYear; currentYear++) {
+        // check if current year divisible by 4
         if (currentYear % 4 === 0 ){
+            // check if current year divisible by 100
             if (currentYear % 100 === 0) {
+                // check if current year divisible by 400
                 if (currentYear % 400 === 0) {
                     leapYear.push(currentYear);
                 }
@@ -11,5 +15,6 @@ function isLeapYear(startYear, endYear) {
             }        
         }
     }
+    
     return leapYear;
 }
