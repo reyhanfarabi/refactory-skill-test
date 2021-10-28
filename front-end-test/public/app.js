f5a115e6cbc598b7d80200f59028b69bab23ac4e
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2021-10-28 14:20:31 +0700

    successfully added both google & facebook oAuth using firebase

--- front-end-test/public/app.js
+++ front-end-test/public/app.js
@@ -1,22 +1,57 @@
+// firebase auth
 const auth = firebase.auth();
 
+// store element to display when user is sign in or sign out
 const whenSignIn = document.querySelector('#whenSignIn');
 const whenSignOut = document.querySelector('#whenSignOut');
 
-const googleSignIn = document.querySelector('#googleSignIn');
-const facebookSignIn = document.querySelector('#facebookSignIn');
+// store button element
+const googleButton = document.querySelector('#googleSignIn');
+const facebookButton = document.querySelector('#facebookSignIn');
 const signOutButton = document.querySelector('#signOutButton');
 
 const userWelcome = document.querySelector('#userWelcome');
 
-const provider = new firebase.auth.GoogleAuthProvider();
 
-// Sign In with Google
-googleSignIn.addEventListener('click', () => {
-    auth.signInWithPopup(provider);
-})
 
-// Sign Out
+
+// sign in with google
+const googleSignIn = () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(googleProvider);
+}
+googleButton.addEventListener('click', googleSignIn);
+
+
+// sign in with facebook
+const facebookSignIn = () => {
+    const facebookProvider = new firebase.auth.FacebookAuthProvider();
+    auth.signInWithPopup(facebookProvider);
+}
+facebookButton.addEventListener('click', facebookSignIn);
+
+
+// sign out
 signOutButton.addEventListener('click', () => {
     auth.signOut();
 })
+
+
+
+
+// change ui state
+auth.onAuthStateChanged(user => {
+
+    console.log(user);
+    if (user) {
+        // when sign in
+        whenSignIn.hidden = false;
+        whenSignOut.hidden = true;
+        userWelcome.textContent = `Hello, ${user.displayName}`;
+    } else {
+        // when sign out
+        whenSignIn.hidden = true;
+        whenSignOut.hidden = false;
+        userWelcome.textContent = ``;
+    }
+});
